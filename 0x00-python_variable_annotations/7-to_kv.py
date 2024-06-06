cda4e3a9af32ea2e3ca6ec302b9a0f86f1e3b2ec
kageruka02 <leonkageruka@gmail.com>
2024-06-06 21:33:03 +0300

    string and int/float to tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,4 +5,4 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     "returns tuple the first one as string and second float"
-    return {k, v**2}
+    return (k, v**2)
