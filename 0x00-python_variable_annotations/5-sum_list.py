1a6dd0138f9ac58d2fdbb2cbf169d57e2beb9895
kageruka02 <leonkageruka@gmail.com>
2024-06-06 20:53:44 +0300

    adding float in array

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+from typing import List
 "basic annotations"
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     "return sum of floats in list"
     return sum([num for num in input_list])
