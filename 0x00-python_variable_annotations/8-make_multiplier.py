9ec4802e6c2089aca1598f47ad516076a7881d9b
kageruka02 <leonkageruka@gmail.com>
2024-06-06 22:09:14 +0300

    type annotation introduction

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,7 +4,10 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    "make multiplier"
+
     def multiplier_function(v: float) -> float:
+        "return multiplication"
         return v * multiplier
 
     return multiplier_function
