1295b86b8119328bd939321abeba6a4dbbabb0ce
kageruka02 <leonkageruka@gmail.com>
2024-06-12 13:35:09 +0300

    hello

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 "Python - Async Comprehension"
 import random
 import asyncio
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     "iteration comprehension"
     for i in range(10):
         await asyncio.sleep(1)
