cdff28fdb537381874bdc7e6ced5ac45a96868f7
kageruka02 <leonkageruka@gmail.com>
2024-06-12 13:28:50 +0300

    hello

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"async generator"
+"Python - Async Comprehension"
+import random
 import asyncio
-from random import uniform
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]:
-    """An async generator that yields a random float between 0 and 10"""
-    for _ in range(10):
+async def async_generator() -> Generator(float, None, None):
+    "iteration comprehension"
+    for i in range(10):
         await asyncio.sleep(1)
-        yield uniform(0, 10)
+        yield random.uniform(0, 10)
